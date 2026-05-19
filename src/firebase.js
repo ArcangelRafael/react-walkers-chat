64c2859c09859c82753b555d1e3219acd16338f4
ArcangelRafael <luis17sdm@hotmail.com>
2026-05-18 20:58:48 -0600

    keys FB

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,11 @@
 import firebase from "firebase/app";
 import "firebase/auth";
 
-export const auth = firebase.initializeApp ({
-    apiKey: "AIzaSyDW26ct7DFLSH_-7pSA1FYm11uNqdTzEhU",
-    authDomain: "w41k3r5j01n18697.firebaseapp.com",
-    projectId: "w41k3r5j01n18697",
-    storageBucket: "w41k3r5j01n18697.appspot.com",
-    messagingSenderId: "214058146720",
-    appId: "1:214058146720:web:627c2c9c8859f9fcdfa2c5"
-  }).auth();
+export const auth = firebase.initializeApp({
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID
+}).auth();
